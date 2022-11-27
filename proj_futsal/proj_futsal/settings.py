0206eccddd20eabd9df6970f689f1a308bf651d1
Goro Osawa <gosawa@gmail.com>
2022-11-27 23:11:11 +0900

    modified static file location

--- proj_futsal/proj_futsal/settings.py
+++ proj_futsal/proj_futsal/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'C:/Users/Cora/Documents/Code/Django/proj_futsal/proj_futsal/static/',
+    [os.path.join(BASE_DIR, 'proj_futsal/static')]
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
