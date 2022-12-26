e8b628701d33504f3fcf2fb543783bcb8efc6d50
Goro Osawa <gosawa@gmail.com>
2022-12-26 18:08:14 +0900

    ready to upload

--- proj_futsal/scheduler/forms.py
+++ proj_futsal/scheduler/forms.py
@@ -5,10 +5,18 @@ from .models import Player
 class RegisterPlayerForm(forms.ModelForm):
     class Meta:
         model = Player
-        fields = ['event', 'name', 'player_status', 'attendance_status', 'comment',] 
+        fields = ['event', 
+        'name', 
+        'player_status', 
+        'attendance_status', 
+        'comment',] 
 
 class EditPlayerForm(forms.ModelForm):
     class Meta:
         model = Player
-        fields = ['event', 'name', 'player_status', 'attendance_status', 'comment',]  
+        fields = ['event', 
+        'name', 
+        'player_status', 
+        'attendance_status', 
+        'comment',]  
     
